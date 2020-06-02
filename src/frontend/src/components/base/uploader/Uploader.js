567efa09e502e70e111a0a7cf34ba3e8e5cbaa4f
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-06-02 11:57:24 -0700

    add uploader component

---
+++ src/frontend/src/components/base/uploader/Uploader.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+// Import React FilePond
+import { FilePond, registerPlugin } from "react-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+export default function Uploader() {
+  const [files, setFiles] = useState([]);
+
+  return (
+    <div className="App">
+      <FilePond
+        files={files}
+        allowMultiple={true}
+        onupdatefiles={setFiles}
+        labelIdle='Drag and Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
+    </div>
+  );
+}
---
+++ src/frontend/src/components/base/uploader/Uploader.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Uploader from "./Uploader";
+
+storiesOf("Uploader", module).add("default", () => <Uploader />);
