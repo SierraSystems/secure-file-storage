e74f72c3e3294560074a87e3c1934b22fc7447c6
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-06-02 12:55:06 -0700

    remove comments

--- src/frontend/src/components/base/uploader/Uploader.js
+++ src/frontend/src/components/base/uploader/Uploader.js
@@ -6,8 +6,6 @@ import { FilePond, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
 
 // Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
