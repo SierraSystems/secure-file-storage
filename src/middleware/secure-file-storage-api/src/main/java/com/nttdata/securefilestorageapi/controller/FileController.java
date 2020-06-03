6f9a78cbd537431604be7a5992abe896f9d3b2d2
Kyle Flood <Kyle.Flood@nttdata.com>
2020-06-02 20:45:55 -0700

    S3 Controller
    Quick controller for S3 upload and delete

---
+++ src/middleware/secure-file-storage-api/src/main/java/com/nttdata/securefilestorageapi/controller/FileController.java
@@ -0,0 +1,39 @@
+package com.nttdata.securefilestorageapi.controller;
+
+import com.nttdata.securefilestorageapi.service.AmazonS3ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    @Autowired
+    private AmazonS3ClientService amazonS3ClientService;
+
+    @PostMapping
+    public Map<String, String> uploadFile(@RequestPart(value = "file") MultipartFile file)
+    {
+        this.amazonS3ClientService.uploadFileToS3Bucket(file, true);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "file [" + file.getOriginalFilename() + "] uploading request submitted successfully.");
+
+        return response;
+    }
+
+    @DeleteMapping
+    public Map<String, String> deleteFile(@RequestParam("file_name") String fileName)
+    {
+        this.amazonS3ClientService.deleteFileFromS3Bucket(fileName);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "file [" + fileName + "] removing request submitted successfully.");
+
+        return response;
+    }
+}
