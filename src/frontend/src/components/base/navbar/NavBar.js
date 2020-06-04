272c21cc6e711ad2dafa2210939ed3b977e71bf5
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-06-04 12:51:50 -0700

    reduce function loc

--- src/frontend/src/components/base/navbar/NavBar.js
+++ src/frontend/src/components/base/navbar/NavBar.js
@@ -4,31 +4,29 @@ import "bootstrap/dist/css/bootstrap.css";
 
 export default function NavBar() {
   return (
-    <div className="blue">
-      <div className="spacing">
-        <nav className="navbar navbar-expand-md sticky-top">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="https://ca.nttdata.com/en/">
-              <img
-                src={`${process.env.PUBLIC_URL}/images/s3.svg`}
-                height="40px"
-                width="55px"
-                alt="s3 logo"
-              />
-            </a>
-          </div>
-          <a className="links" href="https://ca.nttdata.com/en/">
-            About Us
+    <div className="blue spacing">
+      <nav className="navbar navbar-expand-md sticky-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="https://ca.nttdata.com/en/">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/s3.svg`}
+              height="40px"
+              width="55px"
+              alt="s3 logo"
+            />
           </a>
-          <a
-            className="pl-4 links"
-            href="https://github.com/SierraSystems/secure-file-storage"
-          >
-            GitHub
-          </a>
-        </nav>
-        <p className="main-heading">Secure File Upload & Storage</p>
-      </div>
+        </div>
+        <a className="links" href="https://ca.nttdata.com/en/">
+          About Us
+        </a>
+        <a
+          className="pl-4 links"
+          href="https://github.com/SierraSystems/secure-file-storage"
+        >
+          GitHub
+        </a>
+      </nav>
+      <p className="main-heading">Secure File Upload & Storage</p>
     </div>
   );
 }
