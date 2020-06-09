40531584d862b49a6aaaf2c020c69eff04a269ea
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-06-09 15:27:55 -0700

    fix some more linting for the last time

--- src/frontend/src/components/base/download/Download.js
+++ src/frontend/src/components/base/download/Download.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { Files } from "../files/Files";
 import "bootstrap/dist/css/bootstrap.css";
 
